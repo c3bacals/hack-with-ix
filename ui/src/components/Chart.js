6f016825e6e7cf9e135a1880aa89c2db94572455
chriskopher <chriskopher@gmail.com>
2016-11-19 23:31:54 -0500

    Updated Chart.js component to use Recharts package

--- ui/src/components/Chart.js
+++ ui/src/components/Chart.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import NVD3Chart from "react-nvd3";
-import d3 from "d3";
 
 import { Row } from 'components/Flex'
+
+import {ResponsiveContainer, BarChart, LineChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Bar, Line} from 'recharts'
 
 var TestBarData = [{
   key: "Cumulative Return",
@@ -64,31 +64,123 @@ var TestLineData = [{
   ]
 }];
 
+var TestLineData2 = [
+  {
+    "hour" : 0,
+    "value" : 12
+  },
+  {
+    "hour" : 1,
+    "value" : 13
+  },
+  {
+    "hour" : 2,
+    "value" : 16
+  },
+  {
+    "hour" : 3,
+    "value" : 20
+  },
+];
+
+var TestBarData2 = [
+  {
+    "label" : "A" ,
+    "value" : -29.765957771107
+  } ,
+  {
+    "label" : "B" ,
+    "value" : 0
+  } ,
+  {
+    "label" : "C" ,
+    "value" : 32.807804682612
+  } ,
+  {
+    "label" : "D" ,
+    "value" : 196.45946739256
+  } ,
+  {
+    "label" : "E" ,
+    "value" : 0.19434030906893
+  } ,
+  {
+    "label" : "F" ,
+    "value" : -98.079782601442
+  } ,
+  {
+    "label" : "G" ,
+    "value" : -13.925743130903
+  } ,
+  {
+    "label" : "H" ,
+    "value" : -5.1387322875705
+  }
+]
+
+
 export default class Chart extends Component {
   render () {
     return (
-      <div style={{width: "55%"}}>
-        <BarChart data={this.props.platformVsCost} x="label" y="value"></BarChart>
+      <div>
+        <ResponsiveContainer>
+          <LineChart data={TestLineData2} margin={{top: 30, right: 90, bottom: 30, left: 30}}>
+            <XAxis dataKey="hour" label="Hours" />
+            <YAxis label="Spend"/>
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" name="Spend" dataKey="value" stroke="#8884d8" />
+          </LineChart>
+        </ResponsiveContainer>
 
-        <LineChart data={this.props.timeToCost} x="hour" y="value"></LineChart>
+        <ResponsiveContainer>
+          <BarChart data={TestBarData2} margin={{top: 30, right: 90, bottom: 30, left: 30}}>
+            <XAxis dataKey="label"/>
+            <YAxis/>
+            <CartesianGrid strokeDasharray="3 3"/>
+            <Tooltip/>
+            <Legend />
+            <Bar name="Spend" dataKey="value" fill="#8884d8" />
+          </BarChart>
+        </ResponsiveContainer>
       </div>
 
     )
+
   }
 }
 
-export class LineChart extends Component {
+export class LineChartWrapper extends Component {
   render() {
     return (
-      <NVD3Chart type="lineChart" datum={this.props.data} x={this.props.x} y={this.props.y} useInteractiveGuideline={true} />
+      <ResponsiveContainer>
+        <LineChart data={this.props.data} margin={{top: 30, right: 90, bottom: 30, left: 30}}>
+          <XAxis dataKey={this.props.xKey} label={this.props.xKey} />
+          <YAxis label={this.props.yKey}/>
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" name={this.props.xKey} dataKey={this.props.yKey} stroke="#8884d8" />
+        </LineChart>
+      </ResponsiveContainer>
     );
   }
 }
 
-export class BarChart extends Component {
-  render() {
-    return (
-      <NVD3Chart type="discreteBarChart" datum={this.props.data} x={this.props.x} y={this.props.y}/>
-    );
-  }
+export class BarChartWrapper extends Component {
+render() {
+  return (
+    <ResponsiveContainer>
+      <BarChart data={this.props.data} margin={{top: 30, right: 90, bottom: 30, left: 30}}>
+        <XAxis dataKey={this.props.xKey} label={this.props.xKey} />
+        <YAxis label={this.props.yKey}/>
+        <CartesianGrid strokeDasharray="3 3"/>
+        <Tooltip/>
+        <Legend />
+        <Bar name={this.props.xKey} dataKey={this.props.yKey} fill="#8884d8" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+}
 }
