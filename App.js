6b79b555c8cacfa31c436286e7fdfbd6138cbe4c
Apatran <andrei.patranoiu@mail.utoronto.ca>
2016-11-20 00:09:54 -0500

    Impressions Problems

---
+++ App.js
@@ -0,0 +1,57 @@
+// Dependencies
+
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+// Components
+
+import { Center } from 'components/Flex'
+import ModalG from 'components/ModalG'
+import { MyHeader } from 'components/Header.js'
+import { MyFooter } from 'components/Footer.js'
+import { Impressions } from 'components/Impressions.js'
+
+export default class App extends Component {
+      constructor(props) {
+    super(props);
+    this.state = {
+      showComponent_Main1: true,
+      outputAnswer: "empty",
+      
+    };
+  }
+
+  getOutput(output){
+    alert("MY OUTPUT IS: " + output)
+    if(output == "Banner")
+    this.state.outputAnswer = "banner";
+  
+     this.forceUpdate();
+
+  }
+
+
+  renderImpressions(){
+    if(this.state.outputAnswer != "empty")
+    return <Impressions param={this.state.outputAnswer}/>
+
+
+
+  }
+
+    render() {
+      return (
+        <div style={{width:"100%",height:"100%"}}>
+        <MyHeader/>
+        <ModalG getOutput={this.getOutput.bind(this)}/>
+        
+        
+        {this.renderImpressions()}
+        
+        <MyFooter/>
+        </div>
+      )
+    }
+  }
+
+  
